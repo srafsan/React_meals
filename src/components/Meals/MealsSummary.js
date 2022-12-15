152d3ce6fd843869977c1aae247eaac3fd1080f0
srafsan <srafsan98@tutanota.com>
2022-12-15 22:39:09 +0600

    Add Meals Component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious Food, Delivered To You</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eveniet fugiat saepe facilis neque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eveniet fugiat saepe facilis neque!
+            </p>
+        </section>
+    );
+};
+
+export default MealsSummary;
