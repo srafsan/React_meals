994b77384ed05b1c3b4d659c68434ef28ab499dc
srafsan <srafsan98@tutanota.com>
2022-12-16 11:59:59 +0600

    Add Model using React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Model from "../UI/Model/Model";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -12,17 +13,17 @@ const Cart = (props) => {
     );
 
     return (
-        <div>
+        <Model>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
-            <div>
+            <div className={classes.actions}>
                 <button className={classes["button-alt"]}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Model>
     );
 };
 
