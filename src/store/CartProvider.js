018bb9060e2744a550a20a779f6748bbd24d7ed8
srafsan <srafsan98@tutanota.com>
2022-12-21 10:52:50 +0600

    Adding a cart context

---
+++ src/store/CartProvider.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
+
+// const cartReducer
+
+const CartProvider = (props) => {
+    const addItemFromCartHandler = (item) => {};
+    const removeItemFromCartHandler = (id) => {};
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: addItemFromCartHandler,
+        removeItem: removeItemFromCartHandler,
+    };
+    return (
+        <CartContext.Provider value={cartContext}>
+            {props.children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
